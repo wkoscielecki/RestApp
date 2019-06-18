4862376f12ffcfb1111cd636802414fc58e2d90a
wkoscielecki <wojciech11@poczta.onet.pl>
2019-06-18 19:28:23 +0200

    fixtures

--- src/main/java/com/koscielecki/app/Fixtures/UserFixtures.java
+++ src/main/java/com/koscielecki/app/Fixtures/UserFixtures.java
@@ -20,26 +20,26 @@ public class UserFixtures {
     public List<User> createUsers(){
         List<User>users=new ArrayList<>();
         User user1=new User();
-        user1.setName("Chuj");
-        user1.setLastName("Ci");
+        user1.setName("qwe");
+        user1.setLastName("qwe");
         user1.setEmail("naimie@onet.pl");
         user1.setPassword("aa");
         user1.setEnabled(1);
         userRepository.save(user1);
 
         User user2=new User();
-        user2.setName("gowno");
-        user2.setLastName("Ci");
-        user2.setEmail("naimie@onet.pl");
-        user2.setPassword("aa");
+        user2.setName("qwe");
+        user2.setLastName("wewewe");
+        user2.setEmail("nwee@onet.pl");
+        user2.setPassword("awewe");
         user2.setEnabled(1);
         userRepository.save(user2);
 
         User user3=new User();
-        user3.setName("Kurwa");
-        user3.setLastName("Ci");
-        user3.setEmail("naimie@onet.pl");
-        user3.setPassword("aa");
+        user3.setName("weqwe");
+        user3.setLastName("wewq");
+        user3.setEmail("weqe@onet.pl");
+        user3.setPassword("w");
         user3.setEnabled(1);
         userRepository.save(user3);
 
